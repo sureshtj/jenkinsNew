e6a695ea45204b160c61bc7e480a7def57b4e754
sureshtj <32847424+sureshtj@users.noreply.github.com>
2017-11-22 11:37:44 +0530

    Add files via upload

---
+++ DemoA.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class DemoA {
+	public static void main(String[] args)
+	{
+	String s1="ALLSTATE";
+	for(int i=s1.length()-1;i>=0;i--)
+	{
+		System.out.println(s1.charAt(i));
+	}
+	}
+
+}
